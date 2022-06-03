5c9f8e604a17436a950b0216d62902c815f41bab
Moh Subhan <subkh4n@gmail.com>
2022-06-03 10:08:43 +0700

    first commit

---
+++ FE-19/Pertemuan4-5/app.js
@@ -0,0 +1,9 @@
+const jmlAngkot = 10;
+
+for (let i = 1; i <= jmlAngkot; i++) {
+  if (i % 2 == 1) {
+    console.log("Kendaraan angkot no." + i + " ber operasi dengan baik");
+  } else {
+    console.log("Kendaraan angkot no." + i + " tidak ber operasi dengan baik");
+  }
+}
